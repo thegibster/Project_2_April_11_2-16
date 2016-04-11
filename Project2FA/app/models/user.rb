806bdca3eae9db6a702fd29745acef36687e72f0
andrew gibbs <andrewgibbs@outlook.com>
2016-04-11 11:38:09 -0700

    add sass gem user methods from diagram bcrypt gem

--- Project2FA/app/models/user.rb
+++ Project2FA/app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ActiveRecord::Base
   has_many :subscriptions, foreign_key: :follower_id, dependent: :destroy
   has_many :leaders, through: :subscriptions
+  has_many :reverse_subscriptions, foreign_key: :leader_id,class_name: 'Subscription',dependent: :destroy
+  has_many :followers, through: :reverse_subscriptions
+  has_many :posts, dependent: :destroy
+  has_many :text_posts, dependent: :destroy
+  has_many :image_posts, dependent: :destroy
+  has_many :comments
+
+  def following?(leader)
+    leaders.include? leader
+  end
+  def follow!(leader)
+    if leader != self && !following?(leader)
+      leaders << leader
+  end
+  def timeline_user_ids
+    leader_ids + [id]
+
+  end
+
+  has_secure_password
+  validates :email, presence :true, uniqueness: true
 end
