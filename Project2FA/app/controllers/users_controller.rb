21e182e51e7a1eee30ec6dbaba672c74469f075d
andrew gibbs <andrewgibbs@outlook.com>
2016-04-11 11:12:19 -0700

    editing controlls

--- Project2FA/app/controllers/users_controller.rb
+++ Project2FA/app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name)
+      params.require(:email).permit(:email, :password, :password_confirmation, :name)
     end
 end
